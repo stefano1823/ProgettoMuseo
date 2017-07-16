7e981ccc6181eae842f322108e2eb02df7fdf5b2
stefano1823 <netti.stef@gmail.com>
2017-07-16 15:18:38 +0200

    Add files via upload

--- core.php
+++ core.php
@@ -5,12 +5,7 @@ $db_username = 'onlinemuseum';
 $db_password = '';
 $db_name = 'my_onlinemuseum';
 
-$dbConn = new mysqli($db_hostname, $db_username, $db_password,$db_name);
-if (!$dbConn) {
-	echo "Impossibile connettersi al database!";
-	break;
-}
-mysql_select_db($db_name, mysql_connect($db_hostname, $db_username, $db_password));
+mysql_select_db($db_name, mysql_pconnect($db_hostname, $db_username, $db_password));
 mysql_query("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(16) NOT NULL, password VARCHAR(32) NOT NULL, email VARCHAR(60) NOT NULL, reg_ip VARCHAR(20), last_ip VARCHAR(20), reg_date INT NOT NULL, last_login INT)");
 function clear($var) {
 	return addslashes(htmlspecialchars(trim($var)));
