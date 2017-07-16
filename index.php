799f0816af9883d505db37d05a48b58dade29817
stefano1823 <netti.stef@gmail.com>
2017-07-16 08:51:44 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bootstrap Agency Template</title>
 
+<link rel="stylesheet" href="css/style1.css">
 <!-- Bootstrap -->
-<link rel="stylesheet" href="css/bootstrap.css">
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
